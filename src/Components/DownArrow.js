8b20488ea1b4a0555a0737a1be92bebeb340da83
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-15 20:43:57 -0400

    changed downarrow size

--- src/Components/DownArrow.js
+++ src/Components/DownArrow.js
@@ -4,16 +4,13 @@ import styles from "./DownArrow.module.css";
 export default function DownArrow() {
   return (
     <div className={styles.arrowAnim}>
-      <div className={styles.arrowSliding}>
+      <div className={styles.delay1}>
         <div className={styles.arrow}></div>
       </div>
-      <div className={styles.arrowSliding}>
+      <div className={styles.delay2}>
         <div className={styles.arrow}></div>
       </div>
-      <div className={styles.arrowSliding}>
-        <div className={styles.arrow}></div>
-      </div>
-      <div className={styles.arrowSliding}>
+      <div className={styles.delay3}>
         <div className={styles.arrow}></div>
       </div>
     </div>
